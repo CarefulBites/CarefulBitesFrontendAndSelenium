e2f8d2925bfdcdd5ac1c048392d976e62db5730c
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2022-12-05 14:19:31 +0100

    no-sandbox

--- CarefulBitesSelenium/UnitTest1.cs
+++ CarefulBitesSelenium/UnitTest1.cs
@@ -33,6 +33,7 @@ public class UnitTest1
         var chromeOptions = new ChromeOptions();
 
         chromeOptions.AddArgument("--remote-debugging-port=7000");
+        chromeOptions.AddArgument("--no-sandbox");
 
 #if DEBUG
         _driver = new ChromeDriver(DriverDirectory, chromeOptions); // fast
