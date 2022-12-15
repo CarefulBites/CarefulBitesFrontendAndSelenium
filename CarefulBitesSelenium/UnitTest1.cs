85c805a30c19df3ed27dad4d6d8946e90357173f
mads815g <mads61096@gmail.com>
2022-12-15 13:05:51 +0100

    repaired test, still need scroll into view on getRecipe

--- CarefulBitesSelenium/UnitTest1.cs
+++ CarefulBitesSelenium/UnitTest1.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Threading;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 //using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
 //using OpenQA.Selenium.Edge;
 
 namespace CarefulBitesSelenium;
@@ -33,7 +36,7 @@ public class UnitTest1 {
         _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
 #if DEBUG
-        var url = "http://127.0.0.1:5500/index.html";
+        var url = "https://carefulbites.azurewebsites.net/";
 #endif
 #if RELEASE
         var url = "http://127.0.0.1:9000/index.html";
@@ -48,7 +51,7 @@ public class UnitTest1 {
     }
 
     [TestMethod]
-    public void Test5GetLoginModal() {
+    public void Test1GetLoginModal() {
         _driver?.SwitchTo().ActiveElement();
         var loginButton = _driver?.FindElement(By.XPath("//*[@id=\"login\"]"));
 
@@ -57,65 +60,108 @@ public class UnitTest1 {
         loginButton?.Click();
     }
 
+    [TestMethod]
+    public void Test2Login()
+    {
+        _driver?.SwitchTo().ActiveElement();
+        var login = _driver?.FindElement(By.XPath("//*[contains(@name,'username')]"));
+
+        Assert.IsNotNull(login);
+
+        login?.SendKeys("Admin");
+
+        var loginPassword = _driver?.FindElement(By.XPath("//*[contains(@name,'password')]"));
+
+        Assert.IsNotNull(login);
+
+        loginPassword?.SendKeys("Admin");
+
+        var loginButton = _driver?.FindElement(By.XPath("//*[contains(@aria-label,'Log In')]"));
+
+        Assert.IsNotNull(loginButton);
+
+        loginButton?.Click();
+    }
+
+    [TestMethod]
+    public void Test3GetLogo()
+    {
+        _driver?.SwitchTo().ActiveElement();
+        var sort = _driver?.FindElement(By.ClassName("logo"));
+
+        Assert.IsNotNull(sort);
+
+        sort?.Click();
+    }
+
+    [TestMethod]
+    public void Test4ChangeTheme()
+    {
+        _driver?.SwitchTo().ActiveElement();
+        var theme = _driver?.FindElement(By.Id("theme-button"));
+
+        Assert.IsNotNull(theme);
+
+        theme?.Click();
+        theme?.Click();
+    }
+
+    [TestMethod]
+    public void Test5GetStorageGroup()
+    {
+        _driver?.SwitchTo().ActiveElement();
+        var group = _driver?.FindElement(By.XPath("//*[@id=\"itemGrid\"]/div/div[6]/div/table/tbody/tr[1]/td[1]/div"));
+
+        Assert.IsNotNull(group);
+
+        group?.Click();
+    }
+
+    [TestMethod]
+    public void Test6GetSort()
+    {
+        _driver?.SwitchTo().ActiveElement();
+        var sort = _driver?.FindElement(By.XPath("//*[contains(@aria-label,'Column Amount')]"));
+
+        Assert.IsNotNull(sort);
+
+        sort?.Click();
+    }
+
+    [TestMethod]
+    public void Test7GetSearch()
+    {
+        _driver?.SwitchTo().ActiveElement();
+        var search = _driver?.FindElement(By.ClassName("dx-texteditor-input"));
+
+        Assert.IsNotNull(search);
+
+        search?.SendKeys("Milk");
+
+        var clearInput = _driver?.FindElement(By.ClassName("dx-icon-clear"));
+
+        Assert.IsNotNull(clearInput);
+
+        clearInput?.Click();
+    }
+
     //[TestMethod]
-    //public void Test1GetLogo() {
+    //public void Test8GetRecipes()
+    //{
     //    _driver?.SwitchTo().ActiveElement();
-    //    var sort = _driver?.FindElement(By.ClassName("logo"));
+        
+    //    Actions action = new Actions(_driver);
+    //    action.DragAndDropToOffset(_driver.FindElement(By.XPath("//*[@id=\"itemGrid\"]/div/div[6]/div/table/tbody/tr[1]/td[1]/div")), 0, -250);
+    //    action.Build().Perform();
 
-    //    Assert.IsNotNull(sort);
+    //    var filter = _driver?.FindElement(By.XPath("//*[contains(@data-dx_placeholder,'Milk')]"));
+    //    Assert.IsNotNull(filter);
 
-    //    sort?.Click();
+    //    filter?.Click();
     //}
-
+    
     //[TestMethod]
-    //public void Test2ChangeTheme() {
-    //    _driver?.SwitchTo().ActiveElement();
-    //    var theme = _driver?.FindElement(By.Id("theme-button"));
-
-    //    Assert.IsNotNull(theme);
-
-    //    theme?.Click();
-    //    theme?.Click();
-    //}
-
-    //[TestMethod]
-    //public void Test3AGetStorageGroup() {
-    //    _driver?.SwitchTo().ActiveElement();
-    //    var group = _driver?.FindElement(By.XPath("//*[@id=\"itemGrid\"]/div/div[6]/div/table/tbody/tr[1]/td[1]/div"));
-
-    //    Assert.IsNotNull(group);
-
-    //    group?.Click();
-    //}
-
-    //[TestMethod]
-    //public void Test3BGetSort() {
-    //    _driver?.SwitchTo().ActiveElement();
-    //    var sort = _driver?.FindElement(By.XPath("//*[contains(@aria-label,'Column Amount')]"));
-
-    //    Assert.IsNotNull(sort);
-
-    //    sort?.Click();
-    //}
-
-    //[TestMethod]
-    //public void Test4GetSearch() {
-    //    _driver?.SwitchTo().ActiveElement();
-    //    var search = _driver?.FindElement(By.ClassName("dx-texteditor-input"));
-
-    //    Assert.IsNotNull(search);
-
-    //    search?.SendKeys("appelsiner2");
-
-    //    var clearInput = _driver?.FindElement(By.ClassName("dx-icon-clear"));
-
-    //    Assert.IsNotNull(clearInput);
-
-    //    clearInput?.Click();
-    //}
-
-    //[TestMethod]
-    //public void Test6GetAccountPage() {
+    //public void Test8GetAccountPage() {
     //    _driver?.SwitchTo().ActiveElement();
     //    var loginButton = _driver?.FindElement(By.XPath("//*[contains(@aria-label, 'Create Account')]"));
 
